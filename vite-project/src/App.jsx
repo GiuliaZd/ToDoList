dfb68f2cec1fbf8d8330e33bc0af57e166aee678
GiuliaZd <yuliya.zd@gmail.com>
2023-11-30 15:04:59 +0200

    added delete button

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -12,14 +12,21 @@ function App() {
     setTodos([...todos, todo])
     setTodo({description: '', date: ''}) //clear todo
   }
-
+  const deleteTodo = (row)=>{
+    setTodos(todos.filter((todo, index) => index !== row));
+  }
   return (
     <>
       <input placeholder="Description" name="description" value={todo.description} onChange={inputChanged} />
       <input placeholder="Date" name="date" value={todo.date} onChange={inputChanged} />
       <button onClick={addTodo}>Add</button>
       <table><tbody>
-      {todos.map((todo, index) => <tr key={index}><td>{todo.description}</td><td>{todo.date}</td></tr>)}
+      {todos.map((todo, index) => 
+      <tr key={index}>
+        <td>{todo.description}</td>
+        <td>{todo.date}</td>
+        <td><button onClick={()=>deleteTodo(index)}>Delete</button></td>
+        </tr>)}
       </tbody></table>
     </>
   );
